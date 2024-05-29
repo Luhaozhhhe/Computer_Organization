3dac9a986f7eaec0c5e5e61373a9454d985db77b
Luhaozhhhe <150448469+Luhaozhhhe@users.noreply.github.com>
2024-05-29 23:50:19 +0800

    第五次实验没有实验报告

---
+++ Lab5/memory_sync_data_ram/memory_sync_data_ram.srcs/sources_1/ip/data_ram/data_ram_stub.vhdl
@@ -0,0 +1,39 @@
+-- Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2018.3 (win64) Build 2405991 Thu Dec  6 23:38:27 MST 2018
+-- Date        : Mon May 15 17:57:44 2023
+-- Host        : lxmliu2002 running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub {f:/Vivado
+--               Project/memory_sync_data_ram/memory_sync_data_ram.srcs/sources_1/ip/data_ram/data_ram_stub.vhdl}
+-- Design      : data_ram
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7a200tfbg676-2
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity data_ram is
+  Port ( 
+    clka : in STD_LOGIC;
+    wea : in STD_LOGIC_VECTOR ( 3 downto 0 );
+    addra : in STD_LOGIC_VECTOR ( 7 downto 0 );
+    dina : in STD_LOGIC_VECTOR ( 31 downto 0 );
+    douta : out STD_LOGIC_VECTOR ( 31 downto 0 );
+    clkb : in STD_LOGIC;
+    web : in STD_LOGIC_VECTOR ( 3 downto 0 );
+    addrb : in STD_LOGIC_VECTOR ( 7 downto 0 );
+    dinb : in STD_LOGIC_VECTOR ( 31 downto 0 );
+    doutb : out STD_LOGIC_VECTOR ( 31 downto 0 )
+  );
+
+end data_ram;
+
+architecture stub of data_ram is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clka,wea[3:0],addra[7:0],dina[31:0],douta[31:0],clkb,web[3:0],addrb[7:0],dinb[31:0],doutb[31:0]";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "blk_mem_gen_v8_4_2,Vivado 2018.3";
+begin
+end;
